af64a14981c5b109614774fbe236c70c94bdfab8
JozySouza <josiane.fasou@gmail.com>
2026-08-26 16:28:35 -0300

    Adiciona todas as classes relacionadas a Pessoa.

---
+++ Aluno.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Aluno : Pessoa
+{
+    public string NumeroMatricula { get; set; }
+
+    public Aluno(string nome, string cpf, string email, string numeroMatricula)
+        : base(nome, cpf, email)
+    {
+        NumeroMatricula = numeroMatricula;
+    }
+}
