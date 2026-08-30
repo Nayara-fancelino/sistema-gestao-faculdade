55cb4c193381033ed3d804af8f936612f1a4e816
JozySouza <josiane.fasou@gmail.com>
2026-08-30 18:45:56 -0300

    Adicionar propriedades cursos e boletins à classe Aluno

--- Entity/Aluno.cs
+++ Entity/Aluno.cs
@@ -4,11 +4,15 @@
     public class Aluno : Pessoa
     {
         public string NumeroMatricula { get; set; }
+        public List<Curso> cursos { get; set; }
+        public List<Boletim> boletins { get; set; }
 
         public Aluno(string nome, string cpf, string email, string numeroMatricula)
             : base(nome, cpf, email)
         {
             NumeroMatricula = numeroMatricula;
+            cursos = new List<Curso>();
+            boletins = new List<Boletim>();
         }
     }
 }
