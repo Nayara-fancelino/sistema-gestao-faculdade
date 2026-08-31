162aa64d396a5f7faa9189f50c6ca4ab554faaf7
Bruna Carlota <bru.carlota@yahoo.com.br>
2026-08-30 23:42:50 -0300

    remove tipo curso de matricula

--- Entity/Matricula.cs
+++ Entity/Matricula.cs
@@ -4,7 +4,6 @@ public class Matricula
 	public int Id { get; set; }
 	public Aluno Aluno { get; set; }
 	public Curso Curso { get; set; }
-	public TipoCurso TipoCurso { get; set; } // corrigir tipo de curso para o do enum TipoCurso
 	public Boletim Boletim { get; set; }
 
 	public Matricula(Aluno aluno, Curso curso)
